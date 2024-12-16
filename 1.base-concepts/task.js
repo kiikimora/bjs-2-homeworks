7137beba3e44f7d5598b252d21f29db223834e9e
kiikimora <163686637+kiikimora@users.noreply.github.com>
2024-12-16 22:31:07 +0300

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+
+    const d = Math.pow(b, 2) - 4 * a * c;
+    if (d < 0) {
+        return [];
+    } else if (d === 0) {
+        const root = -b / (2 * a);
+        return [root];
+    } else {
+        const root1 = (-b + Math.sqrt(d)) / (2 * a);
+        const root2 = (-b - Math.sqrt(d)) / (2 * a);
+        return [root1, root2];
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    let monthlyPercent = (percent / 100) / 12;
+    let loanBody = amount - contribution;
+if (loanBody <= 0) {
+        return 0; 
+    }
+    let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+
+    let totalAmount = monthlyPayment * countMonths;
+
+    return Math.round(totalAmount * 100) / 100; 
 }
